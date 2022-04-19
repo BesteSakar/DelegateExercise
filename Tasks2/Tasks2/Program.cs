f2e50d68bc876b2fd7e05f4eb480f390e288ba6c
BesteSakar <bestesakarr@outlook.com>
2022-04-19 21:54:02 +0300

    Delegate exercise
    İçerisinde enum kullanımında olduğu ve delegate ile hız işlemlerini düzenlediğimiz bir proje..

---
+++ Tasks2/Tasks2/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace Tasks2
+{
+    internal class Program
+    {
+
+        delegate int SpeedCar(int speed);
+        static void Main(string[] args)
+        {
+
+
+            SpeedCar car1 = new SpeedCar(printSpeed);
+            Console.WriteLine("Your car model is"+" "+Car_models.Toyota);
+            Console.WriteLine("Your speed is" + " "+printSpeed(120));
+            
+
+        }
+        enum Car_models
+        {
+            Toyota,
+            BMW,
+            Mercedes
+        }
+
+
+
+
+        static int  printSpeed(int speed)
+        {
+            if(speed > 100)
+            {
+                Console.WriteLine("Your speed is high..Please slow down..");
+            }
+            else
+            {
+                Console.WriteLine("Your speed is normal..");
+            }
+            return speed;
+           
+        }
+
+
+    }
+}
